3a239d1bc069871e05f52aacd7802f3d1314f440
brick <brickz28@comcast.net>
2024-02-06 20:09:31 -0600

    - API setup

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/apirules', [RulesController::class, 'testing']);
-Route::post('/register', [ApiAuthController::class, 'register']);
+Route::post('/login', [ApiAuthController::class, 'login']);
