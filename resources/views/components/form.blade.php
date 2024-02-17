dbf0b8e4e63b0edd931d12f48cce0cc8e3a92f02
brick <brickz28@comcast.net>
2024-02-17 13:49:38 -0600

    - upload to test on live server for issue

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -11,10 +11,9 @@
                     <div class="card-body p-12">
                         <!--begin::Form-->
                         <form action="{{$route}}" id="kt_invoice_form" {{$file === 'yes' ? 'enctype=multipart/form-data' : "''" }}
-                        method="{{ strtoupper($method) === 'GET' ? 'GET' : 'POST' }}"
+                        method="{{ strtoupper($method) === 'GET' ? 'GET' : 'POST' }}">
                             @method(strtoupper($method))
-                            @csrf
-                        >
+                            @csrf>
                             <!--begin::Wrapper-->
                             <div class="mb-0">
                                 <!--begin::Row-->
