121767d9db9b41e6e401170aed919e280f7461a6
brick <brickz28@comcast.net>
2024-02-17 21:03:03 -0600

    - remove delete from server action menu

--- resources/views/dashboard/server/index.blade.php
+++ resources/views/dashboard/server/index.blade.php
@@ -29,11 +29,6 @@
                             <a href="{{route('servers.show', $server['id'])}}" class="menu-link px-3">View</a>
                         </div>
                         <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link px-3" data-kt-users-table-filter="delete_row">Delete</a>
-                        </div>
-                        <!--end::Menu item-->
                     </div>
                     <!--end::Menu-->
                 </td>
