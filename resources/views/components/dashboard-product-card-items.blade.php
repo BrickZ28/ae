0f2c86b2e78362aa79d8d701d131104832da5e3e
brick <brickz28@comcast.net>
2024-02-17 20:48:12 -0600

    - show server settings

--- resources/views/components/dashboard-product-card-items.blade.php
+++ resources/views/components/dashboard-product-card-items.blade.php
@@ -1,7 +1,7 @@
 <div class="d-flex align-items-center mb-5">
                                                 <span class="fw-semibold fs-6 text-gray-800 flex-grow-1
                                                 pe-3">{{$label}}
-                                                    {{$setting}}</span>
+                                                    {{$setting}}x</span>
     <i class="ki-duotone ki-check-circle fs-1 text-success">
         <span class="path1"></span>
         <span class="path2"></span>
