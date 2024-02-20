6f760a73639c1d9610d9d0fdb33c24a6cb6cb8e1
BrickZ28 <brickz28@comcast.net>
2024-02-20 10:57:53 -0600

    weekly credits job

---
+++ app/Jobs/ApplyAeCreditsJob.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ApplyAeCreditsJob implements ShouldQueue
+{
+	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle()
+    {
+        // Retrieve all users
+        $users = User::all();
+
+        // Grant 50 ae_credits to each user
+        foreach ($users as $user) {
+            $user->increment('ae_credits', 50);
+        }
+    }
+}
