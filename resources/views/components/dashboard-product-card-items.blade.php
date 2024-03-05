6267ad43e3e7e03da5f20c0e6b13e354f4d00498
brick <brickz28@comcast.net>
2024-03-04 20:45:25 -0600

    - default vals if key not found

--- resources/views/components/dashboard-product-card-items.blade.php
+++ resources/views/components/dashboard-product-card-items.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex align-items-center mb-5">
                                                 <span class="fw-semibold fs-6 text-gray-800 flex-grow-1
-                                                pe-3">{{$label}} {{ $settings[$key] ?? 'UNK' }}x</span>
+                                                pe-3">{{$label}} {{ $settings[$settingKey] ?? 'UNK' }}x</span>
     <i class="ki-duotone ki-check-circle fs-1 text-success">
         <span class="path1"></span>
         <span class="path2"></span>
