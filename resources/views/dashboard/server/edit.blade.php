06735b459ec75b9bbd9fe769d71b3763c9c71075
BrickZ28 <brickz28@comcast.net>
2024-03-05 12:14:22 -0600

    multi files upload ready test

--- resources/views/dashboard/server/edit.blade.php
+++ resources/views/dashboard/server/edit.blade.php
@@ -3,12 +3,8 @@
     <x-breadcrumbs title="Add Settings" parent="server" child="edit" />
     <x-form :route="route('servers.update', $server->id)" method="patch" file="yes">
         <div class="row gx-10 mb-5">
-
-
-
-            <x-file-upload label="Add Settings File" file="file" multiple="yes" />
-
-
+            <!-- Make sure to pass "file[]" for handling multiple file uploads -->
+            <x-file-upload label="Add Settings File" file="file[]" multiple="yes" />
         </div>
     </x-form>
 </x-dashboard.layout>
