e68b035f22b04fdffb5f84243f049a813dcbb615
brick <brickz28@comcast.net>
2024-03-04 21:24:16 -0600

    - default vals if key not found change class

--- resources/views/components/dashboard-product-card-items.blade.php
+++ resources/views/components/dashboard-product-card-items.blade.php
@@ -3,7 +3,9 @@
     <span class="fw-semibold fs-6 text-gray-800 flex-grow-1 pe-3">
         {{$label}} {{ $setting ?? 'UNK' }}x
     </span>
-    {{echo $setting;}}
+    @php
+        echo $setting;
+    @endphp
     <i class="ki-duotone {{ isset($setting) ? 'ki-check-circle text-success' : 'ki-cross-square text-warning' }} fs-1">
         <span class="path1"></span>
         <span class="path2"></span>
