15473ba7a5514041b976ac296e6dbbccdbe0467b
Shift <shift@laravelshift.com>
2024-03-12 14:29:47 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::job(new ApplyAeCreditsJob())->weekly();
+Schedule::job(new ClearQuestionAttemptsJob())->daily();
